770e011b31432b51cbcab49e4ca0da154993ad17
joelvaz99 <joelvaz@ipvc.pt>
2020-10-11 16:20:44 +0100

    testes

--- app/src/main/java/com/e/tp1_pm/MainActivity.kt
+++ app/src/main/java/com/e/tp1_pm/MainActivity.kt
@@ -2,10 +2,16 @@ package com.e.tp1_pm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
