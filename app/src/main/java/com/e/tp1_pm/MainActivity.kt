a540e28c1fe73b3c128a1c5b8c1edc705e856e6c
joelvaz99 <joelvaz@ipvc.pt>
2020-10-11 16:35:06 +0100

    testes v1

--- app/src/main/java/com/e/tp1_pm/MainActivity.kt
+++ app/src/main/java/com/e/tp1_pm/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
@@ -13,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun button(view: View) {
+        var nome = findViewById<TextInputEditText>(R.id.name);
+        var idade = findViewById<TextInputEditText>(R.id.age);
+        Toast.makeText(this, nome.text, Toast.LENGTH_SHORT).show() //Mostra o valor inserido
+        findViewById<TextView>(R.id.ver).setText(nome.text)
 
+    }
 }
